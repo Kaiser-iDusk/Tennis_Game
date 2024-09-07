802e7590593b561a02f5810ab5028caf6a156757
Shirsendu <palshirsendu03@gmail.com>
2024-09-07 18:00:51 +0530

    Deploy 2 player mode.

--- game_engine.cpp
+++ game_engine.cpp
@@ -9,7 +9,9 @@ global_variable float ai_pos_x = 0.f, ai_pos_y = 0.f;
 global_variable float ai_v_y = 0.f;
 
 global_variable float ball_pos_x = 0.f, ball_pos_y = 0.f;
-global_variable float ball_v_x = 0.f, ball_v_y = 0.f;
+global_variable float ball_v_x = 250.f, ball_v_y = 0.f;
+
+global_variable float scale = 1.01f;
 
 static void simulate_game(struct Input* input, float dt) {
     clear_screen(0xdecea9);
@@ -33,8 +35,8 @@ static void simulate_game(struct Input* input, float dt) {
     // physics
     float player_acc_y = 0.f, ai_acc_y = 0.f;
 
-    if (is_down(BUTTON_UP)) player_acc_y += 500.f;
-    if (is_down(BUTTON_DOWN)) player_acc_y -= 500.f;
+    if (is_down(BUTTON_UP)) player_acc_y += 750.f;
+    if (is_down(BUTTON_DOWN)) player_acc_y -= 750.f;
 
     if (is_down(BUTTON_W)) ai_acc_y += 500.f;
     if (is_down(BUTTON_S)) ai_acc_y -= 500.f;
@@ -46,13 +48,17 @@ static void simulate_game(struct Input* input, float dt) {
     ai_acc_y -= (ai_v_y * 1.11f);
 
     // motion equations
-        //player
+        // player
     player_pos_y += ((player_v_y * dt) + (player_acc_y * dt * dt * 0.5));
     player_v_y += (player_acc_y * dt);
         
         // ai
     ai_pos_y += ((ai_v_y * dt) + (ai_acc_y * dt * dt * 0.5));
     ai_v_y += (ai_acc_y * dt);
+
+        // ball
+    ball_pos_x += (ball_v_x * dt);
+    ball_pos_y += (ball_v_y * dt);
     
     // collision mechanics
         // player
@@ -73,6 +79,22 @@ static void simulate_game(struct Input* input, float dt) {
         ai_pos_y = -(render_state.h / 2.f) + border_thickness + 75.f;
         ai_v_y *= -.33f;
     }
+        // ball
+    if ((ball_pos_x + 6.f > player_pos_x - 15.f) && (ball_pos_y + 6.f < player_pos_y + 75.f) && (ball_pos_y - 6.f > player_pos_y - 75.f)) {
+        ball_v_x *= -scale;
+        ball_v_y = 0.5f * player_v_y;
+    }
+    else if ((ball_pos_x - 6.f < ai_pos_x + 15.f) && (ball_pos_y + 6.f < ai_pos_y + 75.f) && (ball_pos_y - 6.f > ai_pos_y - 75.f)) {
+        ball_v_x *= -scale;
+        ball_v_y = 0.5f * ai_v_y;
+    }
+
+    if (ball_pos_y + 6.f > (render_state.h / 2.f) - border_thickness) {
+        ball_v_y *= -1.f;
+    }
+    else if (ball_pos_y - 6.f < -(render_state.h / 2.f) + border_thickness) {
+        ball_v_y *= -1.f;
+    }
 
     // render updated states
         // player
@@ -82,5 +104,5 @@ static void simulate_game(struct Input* input, float dt) {
     draw_rect(ai_pos_x, ai_pos_y, 15.0, 75.0, 0xff00ff);
         
         // ball
-    draw_rect(ball_pos_x, ball_pos_y, 5.5, 5.5, 0x000000);
+    draw_rect(ball_pos_x, ball_pos_y, 6.f, 6.f, 0x000000);
 }
